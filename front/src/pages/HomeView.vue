c5f883abc14b74970f04a16033ab615b796a813c
duheng <duheng@bitforex.com>
2023-11-30 20:57:35 +0800

    update

---
+++ front/src/pages/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import useStore from "../store";
+const { counter } = useStore();
+import TheWelcome from "../components/TheWelcome.vue";
+// 使用storeToRefs可以保证解构出来的数据也是响应式的
+const { count, double } = storeToRefs(counter);
+counter.incrementAsync();
+</script>
+
+<template>
+  <main>
+    <div>count: {{ count }}</div>
+    <div>double: {{ double }}</div>
+    <TheWelcome />
+  </main>
+</template>
